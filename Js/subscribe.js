ba2bf522873ce95fe0ec50a5540acb0bb0017d5c
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-05-18 09:28:57 +0700

    add email valid

--- Js/subscribe.js
+++ Js/subscribe.js
@@ -55,6 +55,25 @@ input2.addEventListener('blur', (event) => {
     }
 });
 
+var error_phone = document.getElementById("error-phone_num");
+input2.addEventListener('input', (event) => {
+    if(input2.value.length <= 9){
+        error_phone.style.display = "block";
+        error_phone.innerHTML = "Phone number length must be greater than 9";
+    }
+    if (input2.value.length >= 14){
+        error_phone.style.display = "block";
+        error_phone.innerHTML = "Phone number length must be less than 12";
+    }
+    if(input2.value.length == 0){
+        error_phone.style.display = "block";
+        error_phone.innerHTML = "Phone number must be filled!!";
+    }
+    if(input2.value)
+
+
+
+
 var email = document.getElementById("email");
 var label3 = document.getElementById("email_label");
 email.addEventListener('focus', (event) => {
@@ -70,8 +89,24 @@ email.addEventListener('blur', (event) => {
 });
 var error_email = document.getElementById("error-email");
 email.addEventListener('input', (event) => {
-    if(email.value.length <= 5){
+    if(email.value.includes("@") == false){
+        console.log(error_email);
         error_email.style.display = "block";
-        error_email.innerHTML = "Email length must be greater than 5";
+        error_email.innerHTML = "Email must have @!!";
+    }
+    if(email.value.includes(".") == false){
+        error_email.style.display = "block";
+        error_email.innerHTML = "Email must have .!";
+    }
+    if(email.value.length == 0){
+        error_email.style.display = "block";
+        error_email.innerHTML = "Email must be filled!!";
+    }
+    if(email.value.indexOf(" ") >= 0){
+        error_email.style.display = "block";
+        error_email.innerHTML = "Email can't be space!!";
+    }
+    if(email.value.includes("@") == true && email.value.includes(".") == true && email.value.indexOf(" ") == -1){
+        error_email.style.display = "none";
     }
 });
