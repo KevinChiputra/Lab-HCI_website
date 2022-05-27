16413ae5c64440739cc0a28b280543ced188cf6f
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-05-27 16:33:10 +0700

    [add] responsive page not done

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,21 @@
-const join = async () => {
-	const email = document.querySelector("#email").value;
-	const emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// const join = async () => {
+// 	const email = document.querySelector("#email").value;
+// 	const emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-	if (!emailregex.test(email)) {
-		return alert("Invalid email");
-	}
-	return alert("We will send you an email for information update from us");
-};
+// 	if (!emailregex.test(email)) {
+// 		return alert("Invalid email");
+// 	}
+// 	return alert("We will send you an email for information update from us");
+// };
+
+const menu = document.querySelector(".navbar-right");
+
+const handleMenu = () => {
+    menu.style.display = "flex";
+    console.log("handleMenu");
+}
+
+const closeMenu = () => {
+    menu.style.display = "none";
+    console.log("closeMenu");
+}
